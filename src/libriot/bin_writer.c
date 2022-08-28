06cc3a42d75645de4c0711bd5dcf871a8c40d8c4
Mikołaj Lenczewski <mblenczewski@gmail.com>
2022-08-28 13:14:57 +0100

    Overwrite passed in buf_t with memory managed by bin_writer

--- src/libriot/bin_writer.c
+++ src/libriot/bin_writer.c
@@ -7,5 +7,10 @@ riot_bin_try_write(struct riot_bin const *src, struct mem_t *out) {
 
 	enum riot_io_error err = RIOT_IO_ERROR_OK;
 
+	struct mem_t buf = {0};
+
+	*out = buf;
+
+failure:
 	return err;
 }
